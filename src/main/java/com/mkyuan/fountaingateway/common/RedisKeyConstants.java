f3cd99e561cd5fc41c0f9726fc80cef351e9a655
mk.yuan <mk.yuan@datsch.com.cn>
2025-10-03 01:55:24 +0800

    20251003

--- src/main/java/com/mkyuan/fountaingateway/common/RedisKeyConstants.java
+++ src/main/java/com/mkyuan/fountaingateway/common/RedisKeyConstants.java
@@ -2,8 +2,8 @@ package com.mkyuan.fountaingateway.common;
 
 public class RedisKeyConstants {
 
-    public  static final String Gateway_Router_Definition_Key="com:mkyuan:apigateway:routedefinition";
+    public  static final String Gateway_Router_Definition_Key="com:mkyuan:venus:routedefinition";
     public static final String GATEWAY_ROUTE_CHANGE_TOPIC = "gateway:routes:changes";
 
-    public static final String GATEWAY_USER_LOGIN="gateway:login:users:";//后跟loginId
+    public static final String GATEWAY_USER_LOGIN="venus:login:users:";//后跟loginId
 }
